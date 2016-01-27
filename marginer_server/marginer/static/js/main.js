e84c6fad2ee77e244650da00faba33925edd6aa3
margin <990768144@qq.com>
2016-01-27 22:29:18 +0800

    ，

--- marginer_server/marginer/static/js/main.js
+++ marginer_server/marginer/static/js/main.js
@@ -1,1 +1,76 @@
-alert(1);
+$(document).ready(function(){
+	$.ajax({
+        url:'/get_sp_date',
+        type : 'get',
+        data : {
+        },
+        async: true,//设置为同步
+        dataType : 'json',
+        success:function(data){
+            setCharts(data);
+        },
+        error : function(){
+            alert('error');
+        }
+    });
+	
+});
+
+function setCharts(data){
+	option = {
+	    title : {
+	        text: '温度变化',
+	        subtext: '罗列所有温度相关内容'
+	    },
+	    tooltip : {
+	        trigger: 'axis'
+	    },
+	    legend: {
+	        data:['室内温度','CPU温度','GPU温度']
+	    },
+	    toolbox: {
+	        show : true,
+	        feature : {
+	            dataZoom: {},
+	            dataView: {readOnly: false},
+	            magicType: {type: ['line', 'bar']},
+	            restore: {},
+	            saveAsImage: {}
+	        }
+	    },
+	    xAxis : [
+	        {
+	            type : 'category',
+	            boundaryGap : false,
+	            data : data.time
+	        }
+	    ],
+	    yAxis : [
+	        {
+	            type : 'value',
+	            axisLabel : {
+	                formatter: '{value} °C'
+	            }
+	        }
+	    ],
+	    series : [
+	        {
+	            name:'室内温度',
+	            type:'line',
+	            data:data.house
+	        },
+	        {
+	            name:'CPU温度',
+	            type:'line',
+	            data:data.cpu
+	        },
+	        {
+	            name:'GPU温度',
+	            type:'line',
+	            data:data.gpu
+	        }
+	    ]
+	};
+	temp_chart = echarts.init($('#temp_div')[0]);
+	temp_chart.setOption(option);
+}
--- marginer_server/marginer/views.py
+++ marginer_server/marginer/views.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.http.response import Http404
 from django.shortcuts import render_to_response
-from django.db import connection
+from django.db import connection,transaction
 from models import Info
 from django.http import HttpResponse
-import datetime
+import datetime,time,json
 
 # 获取主页view
 def index(request):
@@ -12,10 +12,30 @@ def index(request):
 
 # 获取树莓派信息
 def get_sp_date(request):
-    print '000000000'
-    info = Info()
-    sp_date_list = Info.objects.all()
-    print '111111111111111'
-    print(sp_date_list.filter(time__range=('2016-01-26', '2016-01-27')))
-    print len(sp_date_list)
-    return HttpResponse('')
+    # 获取当前日期
+    date = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+    cursor = connection.cursor()
+    query_sql = "select * from marginer_info where time like '%"+date+"%' order by time "
+    cursor.execute(query_sql)    #执行sql语句
+    all_info = cursor.fetchall()
+    # 组织温度数据 {"cpu":[],"gpu":[],"house":[],"time":[]}
+    temp_info = {"cpu":[],"gpu":[],"house":[],"humidity":[],"disk":[],"ram":[],"time":[]}
+    for al in all_info:
+        print str(al[8])
+        # 温度相关
+        temp_info["time"].append(str(al[8])[-8:])
+        temp_info["cpu"].append(al[3])
+        temp_info["gpu"].append(al[4])
+        temp_info["house"].append(al[1])
+        
+        # 湿度相关
+        temp_info["humidity"].append(al[2])
+        
+        # 内存使用率
+        temp_info["ram"].append(al[6])
+        
+        # 磁盘使用率
+        temp_info["disk"].append(al[7])
+        
+        
+    return HttpResponse(json.dumps(temp_info), content_type="application/json")
